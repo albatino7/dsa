6999af477daf6bf45c47183da5054afcdc93e9a6
albatino7 <atuldemond1@gmail.com>
2026-07-21 15:17:52 +0530

    ok

---
+++ forLoop.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // printing 1 to 100
+
+    // for (int i = 1; i <= 100; i++)
+    // {
+    //     cout << i << endl;
+    // }
+
+    // printing name 50 times
+
+    // for (int i = 1; i <= 50; i++)
+    // {
+    //     cout << i << " " << "Atul Demond" << endl;
+    // }
+
+    // printing a to z
+
+    for (char ch = 'a'; ch <= 'z'; ch++)
+    {
+        cout << ch << endl;
+    }
+
+    return 0;
+}
--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,13 @@
 using namespace std;
 int main()
 {
-    cout << "Hello world ";
-    cout << endl;
+    // cout << "Hello world ";
+    // cout << endl;
+
+    cout << "Surendra Prasad Singh" << endl;
+    cout << 5 << endl;
+    cout << "2+3" << endl;
+    cout << "2" << endl;
+    int babbar;
     return 0;
 }
