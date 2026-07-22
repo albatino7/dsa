eb57be151cdee20c307957a5ce81b8a7358a9bc7
albatino7 <atuldemond1@gmail.com>
2026-07-22 16:38:48 +0530

    ok

--- 21_07_26.cpp
+++ 21_07_26.cpp
@@ -3,6 +3,10 @@ using namespace std;
 
 int main()
 {
-    cout << "Atul Demond";
+    cout << "Atul Demond" << endl;
+    cout << 5 << endl;
+    cout << "5" << endl;
+    cout << (3 + 3) << endl;
+    cout << "3 + 3" << endl;
     return 0;
 }
---
+++ 22_07_2026.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int age = 23;
+    if (age > 18)
+    {
+        cout << "You Got A licnesed";
+    }
+
+    return 0;
+}
